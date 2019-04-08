62b4549cf0d5f37528d898b1514a0fbbb21be0e2
ManuelRios18 <manuelrios@usantotomas.edu.co>
2019-04-08 18:51:54 -0500

    Add main.cpp

---
+++ src/robot_toolkit/src/main.cpp
@@ -0,0 +1,54 @@
+//======================================================================//
+//  This software is free: you can redistribute it and/or modify        //
+//  it under the terms of the GNU General Public License Version 3,     //
+//  as published by the Free Software Foundation.                       //
+//  This software is distributed in the hope that it will be useful,    //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of      //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE..  See the      //
+//  GNU General Public License for more details.                        //
+//  You should have received a copy of the GNU General Public License   //
+//  Version 3 in the file COPYING that came with this distribution.     //
+//  If not, see <http://www.gnu.org/licenses/>                          //
+//======================================================================//
+//                                                                      //
+//      Copyright (c) 2019 SinfonIA Pepper RoboCup Team                 //
+//      Sinfonia - Colombia                                             //
+//      https://sinfoniateam.github.io/sinfonia/index.html              //
+//                                                                      //
+//======================================================================//
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+
+int main(int argc, char **argv)
+{
+  
+  ros::init(argc, argv, "talker");
+  ros::NodeHandle n;
+
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+
+  ros::Rate loop_rate(10);
+  int count = 0;
+  while (ros::ok())
+  {
+
+    std_msgs::String msg;
+
+    std::stringstream ss;
+    ss << "hello world " << count;
+    msg.data = ss.str();
+
+    ROS_INFO("%s", msg.data.c_str());
+
+    chatter_pub.publish(msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+
+    ++count;
+  }
+
+
+  return 0;
+}
+
